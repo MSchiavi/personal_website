0f33e89311fd74d922b34ab5574589d239603388
MSchiavi <mmschiav@buffalo.edu>
2020-04-19 16:48:46 -0400

    removed some un-needed code

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import Menu from './Menu';
-import About from './About';
 import Literature from './Literature';
 import Home from './Home';
-import Projects from './Projects'
 import Education from './Education'
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
@@ -14,9 +12,7 @@ function App() {
     <div className = "App">
       <Switch>
       <Route path ="/" exact component={Home}/>
-      <Route path ="/about" component={About}/>
       <Route path ="/literature" component={Literature} />
-      <Route path ="/projects" component={Projects} />
       <Route path ="/education" component={Education} />
       </Switch>
       <Menu />
