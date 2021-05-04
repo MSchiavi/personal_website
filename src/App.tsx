881f61fc68d7d0a195ffb71f0fa5a7fb778e860e
MSchiavi <mmschiav@buffalo.edu>
2021-05-03 22:08:58 -0600

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
-import React from 'react';
-import Literature from './views/literature/Literature';
-import Home from './views/home/Home';
-import Education from './views/education/Education'
-import './App.css';
-import NavMenu from './views/index'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React from "react";
+import Literature from "./views/literature/Literature";
+import Home from "./views/home/Home";
+import Education from "./views/education/Education";
+import "./App.css";
+import NavMenu from "./views/index";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
+  //test
   return (
     <Router>
-    <div className = "App" style={{backgroundColor:'rgb(64,78,124)',height:'100vh'}}>
-      <NavMenu />
-      <Switch>
-      <Route path ="/" exact component={Home}/>
-      <Route path ="/literature" component={Literature} />
-      <Route path ="/education" component={Education} />
-      </Switch>
-    </div>
+      <div className="App" style={{ backgroundColor: "rgb(64,78,124)", height: "100vh" }}>
+        <NavMenu />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/literature" component={Literature} />
+          <Route path="/education" component={Education} />
+        </Switch>
+      </div>
     </Router>
   );
 }
